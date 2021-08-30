593c6f53c63b434423f6abbf624e4eb7f35bca07
shaharkamay <shahar.kamay@gmail.com>
2021-08-30 18:45:42 +0300

    updated exercise 1

--- exercise1-30082021.js
+++ exercise1-30082021.js
@@ -9,11 +9,12 @@ function getMedianOrgan(arr) {
     return arr[Math.floor(arr.length/2)];
 }
 
-function CaompareNumbers(a, b) {
+function compareNumbers(a, b) {
     return a - b;
 }
 
 const grades =  [73, 91, 88, 56, 62, 84, 95, 96, 88, 92];
-grades.sort(CaompareNumbers);
-let halfArray = grades.slice(Math.floor(grades.length/2))
+let halfArray = grades.slice(Math.floor(grades.length/2));
+grades.sort(compareNumbers);
+halfArray.sort(compareNumbers);
 console.log("Stats:\n\tRange: " + getMaxRange(grades) + "\n\tMedian: " + getMedianOrgan(grades) + "\n\tHalf Range: " + getMaxRange(halfArray));
