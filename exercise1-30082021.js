4f7dd9dcb4f0ed21e128cc4644c606b0e98cee38
shaharkamay <shahar.kamay@gmail.com>
2021-08-30 18:33:20 +0300

    completed exercise 1

--- exercise1-30082021.js
+++ exercise1-30082021.js
@@ -1,5 +1,19 @@
-// "use strict";
-const grades = [73, 91, 88, 56, 62, 84, 95, 96, 88, 92];
-let maxGrade = Math.max(...grades);
-let minGrade = Math.min(...grades);
-console.log("Stats:\n\tRange: " + maxGrade - minGrade + "\n\tMedian: " + "" + "\n\tHalf Range: " + "");
+"use strict";
+
+function getMaxRange(arr) {
+
+    return (arr[arr.length - 1] - arr[0]);
+}
+
+function getMedianOrgan(arr) {
+    return arr[Math.floor(arr.length/2)];
+}
+
+function CaompareNumbers(a, b) {
+    return a - b;
+}
+
+const grades =  [73, 91, 88, 56, 62, 84, 95, 96, 88, 92];
+grades.sort(CaompareNumbers);
+let halfArray = grades.slice(Math.floor(grades.length/2))
+console.log("Stats:\n\tRange: " + getMaxRange(grades) + "\n\tMedian: " + getMedianOrgan(grades) + "\n\tHalf Range: " + getMaxRange(halfArray));
