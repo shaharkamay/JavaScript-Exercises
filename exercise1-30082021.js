4720d8e47d43230309b39e94dbd45e2045ccbe97
shaharkamay <shahar.kamay@gmail.com>
2021-08-30 17:35:53 +0300

    added exercise 1

---
+++ exercise1-30082021.js
@@ -0,0 +1,5 @@
+// "use strict";
+const grades = [73, 91, 88, 56, 62, 84, 95, 96, 88, 92];
+let maxGrade = Math.max(...grades);
+let minGrade = Math.min(...grades);
+console.log("Stats:\n\tRange: " + maxGrade - minGrade + "\n\tMedian: " + "" + "\n\tHalf Range: " + "");
