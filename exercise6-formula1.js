1449b5250222589622a708608b29eb20c51a69f3
shaharkamay <shahar.kamay@gmail.com>
2021-08-31 20:17:04 +0300

    added exercise6-formula1

---
+++ exercise6-formula1.js
@@ -0,0 +1,32 @@
+"use strict";
+// list of Formula 1 champions since the year 2000
+const formula1Champions = [
+    "Schumacher",
+    "Schumacher",
+    "Schumacher",
+    "Schumacher",
+    "Schumacher",
+    "Alonso",
+    "Alonso",
+    "Räikkönen",
+    "Hamilton",
+    "Button",
+    "Vettel",
+    "Vettel",
+    "Vettel",
+    "Vettel",
+    "Hamilton",
+    "Hamilton",
+    "Rosberg",
+    "Hamilton",
+    "Hamilton",
+    "Hamilton",
+    "Hamilton"
+]
+let winners = {}
+
+function countWin(driver) {
+    // your code goes here
+}
+
+formula1Champions.forEach(countWin)
